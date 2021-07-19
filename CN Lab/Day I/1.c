bc6a109d87c7fe69fd8bcd7b041516b6eaf5a434
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-19 09:13:54 +0530

    Create 1.c

---
+++ CN Lab/Day I/1.c
@@ -0,0 +1,31 @@
+/*
+
+Write a C program to swap the content of 2 variables entered through thecommand line using function and pointer.
+
+*/
+
+#include <stdio.h>
+void swap(int *x, int *y)  
+{  
+    int temp;  
+  
+    temp = *x;  
+    *x   = *y;  
+    *y   = temp;  
+}  
+
+int main()  
+{  
+    int a, b;  
+  
+    printf("Enter values for a and b\n");  
+    scanf("%d%d", &a, &b);  
+  
+    printf("\n\nBefore swapping: a = %d and b = %d\n", a, b);  
+  
+    swap(&a, &b);  
+  
+    printf("\nAfter swapping: a = %d and b = %d\n", a, b);  
+  
+    return 0;  
+}  
